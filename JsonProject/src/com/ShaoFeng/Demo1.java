ac93dcdf1e210a9fc571d55fda2795e24dc40f20
1170197998 <1170197998@qq.com>
2016-11-23 17:29:47 +0800

    add

---
+++ JsonProject/src/com/ShaoFeng/Demo1.java
@@ -0,0 +1,22 @@
+package com.ShaoFeng;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class Demo1 {
+
+	public static void main(String[] args) {
+		JSONObject jsonObject = new JSONObject();
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObjectArray = new JSONObject();
+		
+		try {
+			jsonObjectArray.put("id", "001");
+			jsonObjectArray.put("name", "xiaoming");
+			jsonObjectArray.put("age", "18");
+			jsonArray.put(jsonObjectArray);
+			jsonObject.put("content", jsonArray);
+			System.out.println(jsonObject.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
